faa08ab4ad1e71937b37092360c8698f9de3d572
vitor.modesto <vitor.modesto@extreme.digital>
2025-07-11 17:59:18 -0300

    melhorando salas

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/getSoundscape/:id', (req, res) => {
   const soundscapes = {
   relaxamentoPraiaChuva: ['/sounds/chuva.mp3', '/sounds/praia.mp3', '/sounds/passaro.mp3'],
   cabanaMontanha: ['/sounds/vento.mp3', '/sounds/fogueira.mp3'],
-  meditacaoProfunda: ['/sounds/bineural_4hz.mp3', '/sounds/musica_calma_2.mp3', '/sounds/vento.mp3'],
-  florestaAmanhecer: ['/sounds/passaro.mp3', '/sounds/vento.mp3'], // Adicionar som de arvore
-  ambienteTerapeutico: ['/sounds/musica_calma_1.mp3', '/sounds/bineural_6hz.mp3', '/sounds/chuva.mp3'], // Diminuit musica
-  refugioNoturno: ['/sounds/fogueira.mp3', '/sounds/vento.mp3', '/sounds/musica_calma_2.mp3'],// Diminuit musica
+  meditacaoProfunda: ['/sounds/fogueira.mp3', '/sounds/fogueira.mp3', '/sounds/bineural_4hz.mp3'],
+  florestaAmanhecer: ['/sounds/vento.mp3', '/sounds/incetos_floresta.mp3'], 
+  ambienteTerapeutico: ['/sounds/musica_calma_2.mp3', '/sounds/passaro.mp3'], // Diminuit musica
+  refugioNoturno: ['/sounds/chuva_janela.mp3', '/sounds/incetos_floresta.mp3'],// Diminuit musica
 };
 
   if (soundscapes[id]) {
